5498ccb11834dd6d4e0481b782ec68bd7b521d8f
Shubham jha <sjha870054@gmail.com>
2023-08-21 12:05:56 +0530

    if, else - conditions

---
+++ 06-if-else-condition.js
@@ -0,0 +1,19 @@
+//  if else condition
+
+// 1.
+let age = 18;
+
+if (age > 18) {
+  console.log("He play a cricket");
+} else {
+  console.log("He don`t play a cricket");
+}
+
+// 2.
+const num = 18;
+
+if (num % 2 == 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
