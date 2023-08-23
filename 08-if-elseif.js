9d83f309fce022dec1a0375438cedac254081db3
Shubham jha <sjha870054@gmail.com>
2023-08-23 22:12:58 +0530

    if-elseif-statement

---
+++ 08-if-elseif.js
@@ -0,0 +1,19 @@
+// if
+// else if
+// else if
+// else if
+// else
+
+let tempInDegree = 15;
+
+if (tempInDegree < 0) {
+  console.log("very cold");
+} else if (tempInDegree < 15) {
+  console.log("Cold");
+} else if (tempInDegree < 25) {
+  console.log("Hot");
+} else if (tempInDegree < 35) {
+  console.log("To hot");
+} else {
+  console.log("very hot bro");
+}
