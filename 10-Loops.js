54a34d652c0b3f808405ef8549b9cd60cdeb37f6
Shubham jha <sjha870054@gmail.com>
2023-08-25 19:18:23 +0530

    loop

---
+++ 10-Loops.js
@@ -0,0 +1,43 @@
+// Loop
+// 1.While loop
+// 2.For loop
+// Do While loop
+
+// 1.While loop
+
+/* let i = 0;
+ while (i < 9) {
+   i++;
+   console.log(i);
+ } */
+
+let num = 10;
+let total = 0;
+// let i = 0;
+while (i <= 10) {
+  total = total + i;
+  i++;
+}
+console.log(total);
+
+// 2. for loop
+
+for (let i = 0; i <= 9; i++) {
+  console.log(i);
+}
+// Sum of 10 natural Number
+let sumNaturalNumber = 10;
+let totalNum = 0;
+for (let i = 1; i <= num; i++) {
+  total = total + 1;
+}
+console.log(total);
+
+// Do While loop
+
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i <= 9);
+console.log(i);
